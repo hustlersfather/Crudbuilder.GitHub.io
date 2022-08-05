12b0965d0b95aaed5ad56ad59465d1e4f634ae36
xmarveloader <75556276+marathonloader@users.noreply.github.com>
2022-08-06 00:34:43 +0100

    Initialize passport

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot(GateContract $gate)
+    {
+        $this->registerPolicies($gate);
+        $guest_perrmission = config('fast.guest_perrmission');
+        Gate::before(function ($user, $ability) use ($guest_perrmission) {
+
+            if (in_array($ability, $guest_perrmission))
+                return true;
+
+            if ($user->isSuperAdmin())
+                return true;
+
+            return null;
+        });
+        if (!$this->app->routesAreCached()) {
+            Passport::routes();
+        }
+    }
+}
